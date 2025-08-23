f94585af871261cb28608a3316813d547e00160c
Maximax60 <maximaxmultiservices@gmail.com>
2025-08-23 00:42:03 -0400

    Update index.js

--- Pages/pages/index.js
+++ Pages/pages/index.js
@@ -23,3 +23,20 @@ export default function Home() {
     </div>
   );
 }
+export default function Home() {
+  return (
+    <div style={{
+      backgroundColor: "black",
+      color: "orange",
+      height: "100vh",
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "center",
+      alignItems: "center",
+      fontFamily: "Arial, sans-serif"
+    }}>
+      <h1>MAXIMAX MULTI SERVICES</h1>
+      <p>MAXIMAX ALWAYS STAY MAXIMIZED</p>
+    </div>
+  );
+}
