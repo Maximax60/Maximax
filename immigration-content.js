4b01215ab9d769f27087575e80da31c275433b0a
Maximax60 <maximaxmultiservices@gmail.com>
2025-10-03 01:50:06 -0400

    Create immigration-content.js

---
+++ immigration-content.js
@@ -0,0 +1,13 @@
+export const immigrationUpdates = [
+  "Nouvo tès sitwayènte kòmanse 20 Oktòb 2025.",
+  "USCIS kapab mande idantifyan rezo sosyal sou kèk aplikasyon.",
+  "EB-2 FY2025 plen; nouvo disponib apati 1 Oktòb 2025.",
+  "Depi 28 Oktòb 2025: pifò peman aksepte elektwonik sèlman.",
+];
+
+export const immigrationNotes = [
+  "Pote ID valab + prèv adrès (2 moso si posib).",
+  "Dokiman etranje yo: pote tradiksyon sètifye.",
+  "Randevou: vini 10 min pi bonè pou w siyen revizyon final.",
+  "WhatsApp ap pi vit pou mizajou rapid.",
+];
